dca5140c740424f6861f78aa105427ab8b69df33
ic <76838137+izcoser@users.noreply.github.com>
2021-12-09 20:34:07 -0300

    Do Day 3

---
+++ 3.py
@@ -0,0 +1,39 @@
+def accumulate_input(line, a):
+    ''' Receives one line of input, increments numbers in the list.'''
+    for i, c in enumerate(line):
+        a[i] += int(c)
+
+def g(a):
+    ''' Receives list of numbers, turns each number into a 0 or 1.'''
+    ''' Parses binary string and returns int.'''
+
+    binary_output = [''] * 12
+    for i, n in enumerate(a):
+        if n > 500:
+            binary_output[i] = '1'
+        else:
+            binary_output[i] = '0'
+
+    return int(''.join(binary_output), 2)
+
+
+with open('input3', 'r') as f:
+    lines = f.read().splitlines()
+
+# Each line has 12 bits.
+# Solution: create a list of 12 numbers (a_11, a_10, ... a_0) each starting at 0.
+#   iterate over the lines, adding the value of each bit to the corresponding number.
+#   In the end, if a_i > number of lines / 2, a_i = 1. Else a_i = 0.
+
+a = [0] * 12
+
+
+#print(lines)
+for line in lines:
+    print(line)
+    accumulate_input(line, a)
+    print(a)
+
+gamma = g(a)
+epsilon = ~gamma & 0xFFF
+print(gamma * epsilon)
---
+++ 3_2.py
@@ -0,0 +1,39 @@
+def most_common_nth_bit(lines, n):
+    ''' Receives lines, returns the most common nth bit from left to right.'''
+    count = 0
+    for line in lines:
+        count += int(line[n])
+
+    if count / len(lines) >= 0.5:
+        return '1'
+    else:
+        return '0'
+
+with open('input3', 'r') as f:
+    lines_a = f.read().splitlines()
+
+lines_b = lines_a.copy()
+
+searching_a = True
+searching_b = True
+
+for i in range(12):
+    nth_bit = i
+    most_common_a = most_common_nth_bit(lines_a, nth_bit)
+    most_common_b = most_common_nth_bit(lines_b, nth_bit)
+    if searching_a:
+        lines_a = [line for line in lines_a if line[i] == most_common_a]
+    
+    if searching_b:
+        lines_b = [line for line in lines_b if line[i] != most_common_b]
+    
+    if len(lines_a) == 1:
+        searching_a = False
+
+    if len(lines_b) == 1:
+        searching_b = False
+
+a = int(lines_a[0], 2)
+b = int(lines_b[0], 2)
+
+print(a * b)
