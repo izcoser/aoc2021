409dbf7abc7b7652bf93fec325188f18f3d0e2d8
ic <76838137+izcoser@users.noreply.github.com>
2021-12-11 00:09:54 -0300

    Do Day 6

---
+++ 6.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def pass_day(fish):
+    ''' Makes a one day progress in the fish array. '''
+    fish -= 1
+    fish_to_add = 0
+    for idx, f in enumerate(fish):
+        if f == -1:
+            fish[idx] = 6
+            fish_to_add += 1
+
+
+    return np.concatenate((fish, np.array([8] * fish_to_add)), axis=0)
+
+with open('input6', 'r') as f:
+    fish = np.fromstring(f.readline().strip(), dtype=np.int32, sep=',')
+
+for i in range(80):
+    fish = pass_day(fish)
+
+print(len(fish))
---
+++ 6_2.py
@@ -0,0 +1,22 @@
+def pass_day(fish):
+    ''' Makes a one day progress in the fish array. '''
+    fish_copy = fish.copy()
+
+    for i in range(7, -1, -1):
+        fish[i] = (i, fish_copy[i + 1][1])
+
+    fish[6] = (6, fish[6][1] + fish_copy[0][1])
+    fish[8] = (8, fish_copy[0][1])
+
+with open('input6', 'r') as f:
+    numbers = [int(i) for i in f.readline().strip().split(',')]
+
+fish = [(i, 0) for i in range(9)]
+
+for idx, _ in enumerate(fish):
+    fish[idx] = (idx, numbers.count(idx))
+
+for i in range(256):
+    pass_day(fish)
+
+print(sum([i[1] for i in fish]))
