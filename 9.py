ffb51ee92ab48b3ff5647a1ff4e8df6b2af5ecc2
ic <76838137+izcoser@users.noreply.github.com>
2021-12-20 19:14:27 -0300

    Do Day 9

---
+++ 9.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def adjacent_points(m, x, y):
+    adj = []
+
+    for i, j in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        try:
+            adj.append(m[abs(x + i), abs(y + j)]) # negative indexes will not throw an IndexError.
+        except IndexError:
+            continue
+
+    return adj
+
+def low_point(m, x, y):
+    lower_or_equal_points = [p for p in adjacent_points(m, x, y) if p <= m[x, y]]
+    return len(lower_or_equal_points) == 0
+
+with open('input9', 'r') as f:
+    lines = f.read().splitlines()
+
+m = np.array([[int(n) for n in line] for line in lines], dtype=np.uint8)
+low_points = []
+
+for i in range(m.shape[0]):
+    for j in range(m.shape[1]):
+        if low_point(m, i, j):
+            low_points.append(m[i, j])
+
+print(sum(low_points) + len(low_points))
---
+++ 9_2.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+N = 10
+
+def adjacent_points(m, x, y):
+    adj = []
+
+    for i, j in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        try:
+            adj.append(m[abs(x + i), abs(y + j)]) # negative indexes will not throw an IndexError.
+        except IndexError:
+            continue
+
+    return adj
+
+def low_point(m, x, y):
+    lower_or_equal_points = [p for p in adjacent_points(m, x, y) if p <= m[x, y]]
+    return len(lower_or_equal_points) == 0
+
+def flood_fill(m, x, y):
+    if m[x][y] >= 9:
+            return
+
+    m[x][y] = N
+
+    if x > 0:
+        flood_fill(m, x - 1, y)
+    if x < m.shape[0] - 1:
+        flood_fill(m, x + 1, y)
+    if y > 0:
+        flood_fill(m, x, y - 1)
+    if y < m.shape[1] - 1:
+        flood_fill(m, x, y + 1)
+
+    return
+
+with open('input9', 'r') as f:
+    lines = f.read().splitlines()
+
+m = np.array([[int(n) for n in line] for line in lines], dtype=np.uint32)
+basins = []
+
+for i in range(m.shape[0]):
+    for j in range(m.shape[1]):
+        if low_point(m, i, j):
+            flood_fill(m, i, j)
+            s = (m == N).sum()
+            if s > 0:
+                basins.append(s)
+            N += 1
+
+basins.sort()
+print(np.prod(basins[-3:]))
